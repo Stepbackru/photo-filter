66b839ad3e49cc295dbabb423adffb2dd2ba9368
Stepbackru <alex-ruba@bk.ru>
2021-04-27 02:05:18 +0300

    feat: add functional for Next Pic

--- components/image.js
+++ components/image.js
@@ -1,22 +1,27 @@
 import timeOfDay from './timeOfDay.js';
 
-const COUNT_IMAGES = 20;
+let count = 0;
 
 const image = () => {
   const img = document.querySelector('img');
   // const randomUrl = `./assets/aviation/${timeOfDay()}/${randomImage(COUNT_IMAGES)}.jpg`;
-  const randomUrl = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay()}/${randomImage(COUNT_IMAGES)}.jpg`;
+  if (count > 19) {
+    count = 1;
+  } else {
+    count++;
+  }
+  const randomUrl = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay()}/${countImage(count)}.jpg`;
   
   fetch(randomUrl)
     .then(res => res.url)
     .then(url => img.src = `${url}`);
 }
 
-const randomImage = (n) => {
-  const rand = Math.floor(Math.random() * n + 1);
-  let number = '' + rand;
-  if (rand < 10) {
-    number = '0' + rand;
+const countImage = (start) => {
+  // const rand = Math.floor(Math.random() * n + 1);
+  let number = '' + start;
+  if (start < 10) {
+    number = '0' + start;
   }
   return number;
 }
--- script.js
+++ script.js
@@ -5,11 +5,12 @@ import image from './components/image.js';
 window.onload = () => {
   const inputs = [...document.querySelectorAll('.filters label input')];
   const bntReset = document.querySelector('.btn-reset');
+  const bntNext = document.querySelector('.btn-next');
 
   inputs.forEach((el) => {
     el.addEventListener('input', outputChange);
   });
 
   bntReset.addEventListener('click', reset);
-  image();
+  bntNext.addEventListener('click', image);
 }
