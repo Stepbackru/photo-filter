bbd47aaa739d7c2b4b522b5b13a697b2950a31c4
Stepbackru <alex-ruba@bk.ru>
2021-04-27 01:31:56 +0300

    feat: add url to gh

--- components/image.js
+++ components/image.js
@@ -1,10 +1,11 @@
 import timeOfDay from './timeOfDay.js';
 
-const COUNT_IMAGES = 5;
+const COUNT_IMAGES = 20;
 
 const image = () => {
   const img = document.querySelector('img');
-  const randomUrl = `./assets/aviation/${timeOfDay()}/${randomImage(COUNT_IMAGES)}.jpg`;
+  // const randomUrl = `./assets/aviation/${timeOfDay()}/${randomImage(COUNT_IMAGES)}.jpg`;
+  const randomUrl = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay()}/${randomImage(COUNT_IMAGES)}.jpg`;
   
   fetch(randomUrl)
     .then(res => res.url)
