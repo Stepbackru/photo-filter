c6ad743b6f2cbf937c711ae110f19a4a83d5b471
Stepbackru <alex-ruba@bk.ru>
2021-04-27 02:27:45 +0300

    feat: add reset styles

--- components/reset.js
+++ components/reset.js
@@ -1,9 +1,12 @@
   const reset = () => {
     const inputs = [...document.querySelectorAll('.filters label input')];
+    const img = document.querySelector('img');
+
 
     inputs.forEach((el) => {
       el.value = el.defaultValue;
       el.nextElementSibling.value = el.value;
+      img.style.setProperty(`--${el.name}`, el.value + el.dataset.sizing);
     });
   }
 
