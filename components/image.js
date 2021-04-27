56f812e94e00f2ad2dabda383a7be76316e47975
Stepbackru <alex-ruba@bk.ru>
2021-04-27 03:27:27 +0300

    fix: save image from url

--- components/image.js
+++ components/image.js
@@ -4,21 +4,20 @@ let count = 0;
 
 const image = () => {
   const img = document.querySelector('img');
-  // const randomUrl = `./assets/aviation/${timeOfDay()}/${randomImage(COUNT_IMAGES)}.jpg`;
   if (count > 19) {
     count = 1;
   } else {
     count++;
   }
   const randomUrl = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay()}/${countImage(count)}.jpg`;
-  
+  img.setAttribute('crossOrigin', 'anonymous');
+
   fetch(randomUrl)
     .then(res => res.url)
     .then(url => img.src = `${url}`);
 }
 
 const countImage = (start) => {
-  // const rand = Math.floor(Math.random() * n + 1);
   let number = '' + start;
   if (start < 10) {
     number = '0' + start;
